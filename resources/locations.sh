e9962ed219e0674f7d3df023fa2c7949edcc99c4
Martin Schwarzer <polyluxus@gmail.com>
2018-10-24 22:28:56 +0200

    Import library modified functions from tools-for-g16

---
+++ resources/locations.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+
+# If this script is not sourced, return before executing anything
+if (( ${#BASH_SOURCE[*]} == 1 )) ; then
+  echo "This script is only meant to be sourced."
+  exit 0
+fi
+
+# 
+# Let's know where the script is and how it is actually called
+#
+
+get_absolute_location ()
+{
+    # Resolves the absolute location of parameter and returns it
+    # Taken from https://stackoverflow.com/a/246128/3180795
+    local resolve_file="$1" description="$2" 
+    local link_target directory_name filename resolve_dir_name 
+    debug "Getting directory for '$resolve_file'."
+    #  resolve $resolve_file until it is no longer a symlink
+    while [[ -h "$resolve_file" ]]; do 
+      link_target="$(readlink "$resolve_file")"
+      if [[ $link_target == /* ]]; then
+        debug "File '$resolve_file' is an absolute symlink to '$link_target'"
+        resolve_file="$link_target"
+      else
+        directory_name="$(dirname "$resolve_file")" 
+        debug "File '$resolve_file' is a relative symlink to '$link_target' (relative to '$directory_name')"
+        #  If $resolve_file was a relative symlink, we need to resolve 
+        #+ it relative to the path where the symlink file was located
+        resolve_file="$directory_name/$link_target"
+      fi
+    done
+    debug "File is '$resolve_file'" 
+    filename="$(basename "$resolve_file")"
+    debug "File name is '$filename'"
+    resolve_dir_name="$(dirname "$resolve_file")"
+    directory_name="$(cd -P "$(dirname "$resolve_file")" && pwd)"
+    if [[ "$directory_name" != "$resolve_dir_name" ]]; then
+      debug "$description '$directory_name' resolves to '$directory_name'."
+    fi
+    debug "$description is '$directory_name'"
+    if [[ -z $directory_name ]] ; then
+      directory_name="."
+    fi
+    echo "$directory_name/$filename"
+}
+
+get_absolute_filename ()
+{
+    # Returns only the filename
+    local resolve_file="$1" description="$2" return_filename
+    return_filename=$(get_absolute_location "$resolve_file" "$description")
+    return_filename=${return_filename##*/}
+    echo "$return_filename"
+}
+
+get_absolute_dirname ()
+{
+    # Returns only the directory
+    local resolve_file="$1" description="$2" return_dirname
+    return_dirname=$(get_absolute_location "$resolve_file" "$description")
+    return_dirname=${return_dirname%/*}
+    echo "$return_dirname"
+}
+
+
+get_scriptpath_and_source_files ()
+{
+    local error_count tmplog line tmpmsg
+    tmplog=$(mktemp tmp.XXXXXXXX) 
+    # Who are we and where are we?
+    scriptname="$(get_absolute_filename "${BASH_SOURCE[0]}" "installname")"
+    debug "Script is called '$scriptname'"
+    # remove scripting ending (if present)
+    scriptbasename=${scriptname%.sh} 
+    debug "Base name of the script is '$scriptbasename'"
+    scriptpath="$(get_absolute_dirname  "${BASH_SOURCE[0]}" "installdirectory")"
+    debug "Script is located in '$scriptpath'"
+    resourcespath="$scriptpath/resources"
+    
+    if [[ -d "$resourcespath" ]] ; then
+      debug "Found library in '$resourcespath'."
+    else
+      (( error_count++ ))
+    fi
+    
+    # Import default variables
+    #shellcheck source=/home/te768755/devel/tools-for-orca.bash/resources/default_variables.sh
+    source "$resourcespath/default_variables.sh" &> "$tmplog" || (( error_count++ ))
+    
+    # Set more default variables
+    exit_status=0
+    stay_quiet=0
+    
+    # Import other functions
+    #shellcheck source=/home/te768755/devel/tools-for-orca.bash/resources/messaging.sh
+    source "$resourcespath/messaging.sh" &> "$tmplog" || (( error_count++ ))
+    #shellcheck source=/home/te768755/devel/tools-for-orca.bash/resources/rcfiles.sh
+    source "$resourcespath/rcfiles.sh" &> "$tmplog" || (( error_count++ ))
+    #shellcheck source=/home/te768755/devel/tools-for-orca.bash/resources/test_files.sh
+    source "$resourcespath/test_files.sh" &> "$tmplog" || (( error_count++ ))
+##??    #shellcheck source=/home/te768755/devel/tools-for-orca.bash/resources/process_orca.sh
+##??    source "$resourcespath/process_orca.sh" &> "$tmplog" || (( error_count++ ))
+    #shellcheck source=/home/te768755/devel/tools-for-orca.bash/resources/validate_numbers.sh
+    source "$resourcespath/validate_numbers.sh" &> "$tmplog" || (( error_count++ ))
+
+    if (( error_count > 0 )) ; then
+      echo "ERROR: Unable to locate library functions. Check installation." >&2
+      echo "ERROR: Expect functions in '$resourcespath'."
+      debug "Errors caused by:"
+      while read -r line || [[ -n "$line" ]] ; do
+        debug "$line"
+      done < "$tmplog"
+      tmpmsg=$(rm -v "$tmplog")
+      debug "$tmpmsg"
+      exit 1
+    else
+      tmpmsg=$(rm -v "$tmplog")
+      debug "$tmpmsg"
+    fi
+}
+
